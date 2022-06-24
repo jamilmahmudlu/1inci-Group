64a7f97d765e20fccea110403a66d6861865c7e0
jamilmahmudlu <jamilmahmudlu@gmail.com>
2022-06-24 13:28:40 +0400

    last

--- goldapp/translation.py
+++ goldapp/translation.py
@@ -1,7 +1,11 @@
 from modeltranslation.translator import register, TranslationOptions
 
-from goldapp.models import Product
+from goldapp.models import Product, Category
 
 @register(Product)
 class ProductTranslationOptions(TranslationOptions):
     fields = ('name',)
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('title',)
